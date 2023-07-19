f956e694fb18a3643e043bfb431e692e5d73ed6a
Yousefkotb2 <yousefkotb2019@gmail.com>
2023-07-19 05:40:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main"
+/**
+ * main - print all alphabet in lowecase
+ */
+int main (void)
+{
+	char letter;
+	for(letter='a'\ ,letter<='z';,letter++)
+	_putchar(letter);
+
+	_putchar('\n');
+}
+
